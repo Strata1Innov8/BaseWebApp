27a55f8396a7b86345eac6a2c3e6f9d960d9a649
Strata1Innov8 <stratacensioninc@gmail.com>
2019-10-15 13:22:49 -0400

    new I said

--- project/js/main.js
+++ project/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 
 function getWeather () {
   var url = "https://api.openweathermap.org/data/2.5/weather?q=Baltimore&APPID="+apiKey;
+  
   $.ajax(url,{success: function(data){
     console.log(data);
   }})
